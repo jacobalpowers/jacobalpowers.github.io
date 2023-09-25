470bd59fe266779d72420fdf58cff9903916618d
Jake Powers <jpowers1324@gmail.com>
2023-09-25 09:10:00 -0400

    Hamburger nav Implemented

--- assignments/assignment7/script.js
+++ assignments/assignment7/script.js
@@ -2,21 +2,25 @@ const exercise_1_click = () => {
     const exercise1 = document.getElementById("exercise_1");
     const exercise2 = document.getElementById("exercise_2");
     exercise1.style.borderBottomColor = "salmon";
-    exercise2.style.borderBottomColor = "white";
-    document.getElementById("whos_the_oldest").style.display("inline");
-    document.getElementById("thermometer").style.display("none");
+    exercise2.style.borderBottomColor = "transparent";
+    document.getElementById("whos_the_oldest").style.display = "table";
+    document.getElementById("thermometer").style.display = "none";
 }
 const exercise_2_click = () => {
     const exercise1 = document.getElementById("exercise_1");
     const exercise2 = document.getElementById("exercise_2");
-    exercise1.style.borderBottomColor = "white";
+    exercise1.style.borderBottomColor = "transparent";
     exercise2.style.borderBottomColor = "salmon";
-    document.getElementById("whos_the_oldest").style.display("none");
-    document.getElementById("thermometer").style.display("inline");
+    document.getElementById("whos_the_oldest").style.display = "none";
+    document.getElementById("thermometer").style.display = "block";
+}
+const toggle_nav = () => {
+    document.getElementById("exercises").classList.toggle("hidden");
 }
 
 
 window.onload = () => {
     document.getElementById("exercise_1").onclick = exercise_1_click;
     document.getElementById("exercise_2").onclick = exercise_2_click;
+    document.getElementById("hamburger_menu").onclick = toggle_nav;
 }
