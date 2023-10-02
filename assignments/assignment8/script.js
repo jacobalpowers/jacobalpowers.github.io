943d5f4bae37e05d8c14dbcb5af1a4a386cb8191
Jake Powers <jpowers1324@gmail.com>
2023-10-02 03:17:25 -0400

    Thermometer complete and Running Fix

--- assignments/assignment8/script.js
+++ assignments/assignment8/script.js
@@ -2,10 +2,13 @@ var goal = 10000;
 var running = false;
 var walk = true;
 var position = 0;
+var currFill = 0;
+var filling = false;
+var maxFill = 0;
 
 const toggleRun = () => {
     if (!running) {
-        running = setInterval(runningMan, 500);
+        running = setInterval(runningMan, 200);
     } else {
         clearInterval(running);
         running = false;
@@ -14,7 +17,12 @@ const toggleRun = () => {
 
 const runningMan = () => {
     const man = document.getElementById("man-img");
-    //man.src = man.src == "img/walkingman.png" ? "img/runningman.png" : "img/walkingman.png";
+    if (position >= 70) {
+        man.src = "img/walkingman.png";
+        clearInterval(running);
+        running = false;
+        return;
+    }
     if (walk) {
         man.src = "img/runningman.png";
         walk = false;
@@ -24,14 +32,45 @@ const runningMan = () => {
     }
     position += 2.5;
     man.style.setProperty("--position", position + "%");
-    if (position > 70) {
-        man.src = "img/walkingman.png";
-        clearInterval(running);
-        running = false;
+    
+}
+
+const is_not_number = (data) => {
+    if (isNaN(data.trim())) {
+        return true;
     }
+    return false;
+}
+
+const fillThermo = () => {
+    const bar = document.getElementById("gradient_bar");
+    currFill += 1;
+    bar.style.setProperty("--complete", currFill + "%");
+    if (currFill >= maxFill) {
+        clearInterval(filling);
+        filling = false;
+    }
+}
+
+const displayThermo = () => {
+    const bar = document.getElementById("gradient_bar");
+    const funds = document.getElementById("funds_raised").value;
+    currFill = 0;
+    maxFill = 0;
+    if (is_not_number(funds)) {
+        console.debug("Funds not number");
+        return false;
+    }
+
+    maxFill = (funds/goal) * 100;
+    if (maxFill > 100) {
+        maxFill = 100;
+    }
+    filling = setInterval(fillThermo, 10);
 }
 
 window.onload = () => {
     document.getElementById("man-img").onclick = toggleRun;
+    document.getElementById("display_funds").onclick = displayThermo;
 }
 
