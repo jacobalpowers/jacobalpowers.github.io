64d5fe82f5514db31b7e7b1b3dec47c73c534fac
Jake Powers <jpowers1324@gmail.com>
2023-10-16 01:06:50 -0400

    Assignment 10 Complete

--- assignments/assignment10/script.js
+++ assignments/assignment10/script.js
@@ -9,7 +9,9 @@ class Toy {
 
     get details() {
         const toySection = document.createElement("section");
+        const toyDescription = document.createElement("div");
         toySection.classList.add("toy-item");
+        toyDescription.classList.add("description");
 
         const toyTitle = document.createElement("p");
         toyTitle.setAttribute("class", "bold");
@@ -17,6 +19,9 @@ class Toy {
 
         const toyImg = document.createElement("img");
         toyImg.setAttribute("src", "images/" + this.img);
+
+        const price = document.createElement("div");
+        price.setAttribute("class", "flex");
 
         const priceTitle = document.createElement("p");
         priceTitle.setAttribute("class", "bold");
@@ -26,6 +31,9 @@ class Toy {
         toyPrice.setAttribute("class", "value");
         toyPrice.innerText = this.price;
 
+        const age = document.createElement("div");
+        age.setAttribute("class", "flex");
+
         const ageTitle = document.createElement("p");
         ageTitle.setAttribute("class", "bold");
         ageTitle.innerText = "Age Range: ";
@@ -33,6 +41,9 @@ class Toy {
         const toyAge = document.createElement("p");
         toyAge.setAttribute("class", "value");
         toyAge.innerText = this.age;
+
+        const rating = document.createElement("div");
+        rating.setAttribute("class", "flex");
 
         const ratingTitle = document.createElement("p");
         ratingTitle.setAttribute("class", "bold");
@@ -42,14 +53,19 @@ class Toy {
         toyRating.setAttribute("class", "value");
         toyRating.innerText = this.rating;
 
-        toySection.appendChild(toyTitle);
+        toyDescription.appendChild(toyTitle);
         toySection.appendChild(toyImg);
-        toySection.appendChild(priceTitle);
-        toySection.appendChild(toyPrice);
-        toySection.appendChild(ageTitle);
-        toySection.appendChild(toyAge);
-        toySection.appendChild(ratingTitle);
-        toySection.appendChild(toyRating);
+        
+        price.appendChild(priceTitle);
+        price.appendChild(toyPrice);
+        toyDescription.appendChild(price);
+        age.appendChild(ageTitle);
+        age.appendChild(toyAge);
+        toyDescription.appendChild(age);
+        rating.appendChild(ratingTitle);
+        rating.appendChild(toyRating);
+        toyDescription.appendChild(rating);
+        toySection.appendChild(toyDescription);
 
         return toySection;
     }
@@ -63,11 +79,13 @@ window.onload = () => {
     let toys = [];
     let toyList = document.getElementById("toy-list");
 
-    toys.push(new Toy("Buzz Lightyear", "buzzlightyear.jpg", "$28", "3-7", "4 Stars"));
     toys.push(new Toy("Blocks", "blocks.jpg", "$5", "1-3", "5 Stars"));
-    toys.push(new Toy("Lego", "legos.jpg", "$50", "5-11", "3.8 Stars"));
-    toys.push(new Toy("Teddy Bear", "teddybear.jpg", "$10", "1-10", "5 Stars"));
+    toys.push(new Toy("Buzz Lightyear", "buzzlightyear.jpg", "$28", "3-7", "4 Stars"));
     toys.push(new Toy("Gundam", "gundam.jpg", "$75", "12+", "2.8 Stars"));
+    toys.push(new Toy("Lego", "legos.jpg", "$50", "5-11", "3.8 Stars"));
+    toys.push(new Toy("Rubber Duck", "rubberduck.jpg", "$14", "2+", "4.9 Stars"));
+    toys.push(new Toy("Teddy Bear", "teddybear.jpg", "$10", "1-10", "5 Stars"));
+    
 
     for (let i in toys) {
         toyList.append(toys[i].details);
