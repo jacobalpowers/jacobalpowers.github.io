2f22c015440a235528831105c72bd9e77a3d9d1f
Jake Powers <jpowers1324@gmail.com>
2024-04-22 00:19:46 -0400

    Mongo integration

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 const joi = require("joi");
+const mongoose = require("mongoose");
 const multer = require("multer");
 app.use(express.static("public"));
 app.use(express.json());
 const cors = require("cors");
 app.use(cors());
+
+mongoose
+    .connect("mongodb+srv://japowers:RUD1IMizJvcIr6Q4@data.wtiy9yq.mongodb.net/")
+    .then(() => console.log("Connected to mongodb..."))
+    .catch((err) => console.error("Could not connect to mongodb...", err));
 
 
 app.get('/', (req, res) => {
