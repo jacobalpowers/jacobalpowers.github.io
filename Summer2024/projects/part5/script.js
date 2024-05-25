141c3691261f6a091526164b453d95ed0c030e65
Jake Powers <jpowers1324@gmail.com>
2024-05-25 13:09:45 -0400

    Update script.js

--- Summer2024/projects/part5/script.js
+++ Summer2024/projects/part5/script.js
@@ -1,9 +1,22 @@
+const url = "https://jacobalpowers.github.io/Summer2024/projects/part5/games.json";
 
+const getGames = async () => {
+    try {
+        const json = (await fetch(url)).json();
+        return json;
+    } catch(error) {
+        console.log(error);
+    }
+}
 
-
-
-
+const displayGames = async () => {
+    const json = await getGames();
+    
+    json.forEach(async (e) => {
+        console.log(e);
+    });
+}
 
 window.onload = () => {
-
+    
 }
