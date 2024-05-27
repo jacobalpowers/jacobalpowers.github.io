fa36b7d0643062854cf979587e2d676640f3b1a4
Jake Powers <jpowers1324@gmail.com>
2024-05-27 01:51:13 -0400

    Project 6 Complete

--- Summer2024/projects/part6/contact/script.js
+++ Summer2024/projects/part6/contact/script.js
@@ -1,7 +1,55 @@
-const url = "https://jacobalpowers.github.io/Summer2024/projects/part5/games.json";
+//https://api.web3forms.com/submit
+const getEmailResult = async () => {
+    const form = document.getElementById("contact-form");
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+    console.log(json);
 
+    const result = document.getElementById("result");
+    result.innerHTML = "Please Wait....";
 
+    try {
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers:{
+                "Content-Type":"application/json",
+                Accept:"application/json"
+            },
+            body:json
+        });
+        return response;
+    } catch(error) {
+        console.log(error);
+        result.innerHTML = "Sorry, your email could not be sent.";
+    }
+}
 
-window.onload = () => {
-    
+/*const showEmailResult = async(e) => {
+    e.preventDefault();
+
+    const result = document.getElementById("result");
+    let response = await getEmailResult();
+
+    if(response.status == 200){
+        result.innerHTML = "Email successfully sent";
+    } else {
+        result.innerHTML = "Sorry, your email was not sent";
+    }
+    return false;
+};
+
+document.getElementById("contact-form").onsubmit = showEmailResult;*/
+
+document.getElementById("contact-form").onsubmit = async(e) => {
+    e.preventDefault();
+
+    const result = document.getElementById("result");
+    let response = await getEmailResult();
+
+    if(response.status == 200){
+        result.innerHTML = "Email successfully sent";
+    } else {
+        result.innerHTML = "Sorry, your email was not sent";
+    }
 }
--- Summer2024/projects/part6/games/script.js
+++ Summer2024/projects/part6/games/script.js
@@ -9,44 +9,63 @@ const getGames = async () => {
     }
 }
 
+//add new form item from form
+const addFormItem = (e) => {
+    e.preventDefault();
+    const form = document.getElementById("new-item");
+    const list = document.getElementById("item-list");
+
+    list.append(createGameItem("placeholder.png", form["game-name"].value, form["release-date"].value, form["game-rank"].value, form.price.value));
+}
+
 const displayGames = async () => {
     const json = await getGames();
     const list = document.getElementById("item-list");
     
+    //display all items in json
     json.forEach(async (e) => {
-        const div = document.createElement("div");
-        div.classList.add("game-item");
-
-        const img = document.createElement("img");
-        img.src = `../images/${e.image}`;
-        div.append(img);
-
-        const text = document.createElement("div");
-        text.id = "text-section";
-
-        const title = document.createElement("p");
-        title.innerHTML = `${e.title}`;
-        text.append(title);
-
-        const release = document.createElement("p");
-        release.innerHTML = `${e["release-date"]}`;
-        text.append(release);
-
-        const ranking = document.createElement("p");
-        ranking.innerHTML = `Rank: ${e.rank}`;
-        text.append(ranking);
-
-        const price = document.createElement("p");
-        price.innerHTML = `$${e.price}`;
-        text.append(price);
-
-        div.append(text);
-        list.append(div);
+        list.append(createGameItem(e.image, e.title, e["release-date"], e.rank, e.price));
     });
+}
+
+//return game item element
+const createGameItem = (img, title, releaseDate, ranking, price) => {
+    const div = document.createElement("div");
+    div.classList.add("game-item");
+
+    const image = document.createElement("img");
+    image.src = `../images/${img}`;
+    div.append(image);
+
+    const text = document.createElement("div");
+    text.id = "text-section";
+
+    const gameTitle = document.createElement("p");
+    gameTitle.innerHTML = `${title}`;
+    text.append(gameTitle);
+
+    const gameRelease = document.createElement("p");
+    gameRelease.innerHTML = `${releaseDate}`;
+    text.append(gameRelease);
+
+    const gameRanking = document.createElement("p");
+    gameRanking.innerHTML = `Rank: ${ranking}`;
+    text.append(gameRanking);
+
+    const gamePrice = document.createElement("p");
+    gamePrice.innerHTML = `$${price}`;
+    text.append(gamePrice);
+    
+    div.append(text);
+    
+    return div;
 }
 
 displayGames();
 
 window.onload = () => {
-
+    document.getElementById("add-item").onclick = () => {
+        document.getElementById("id01").style.display = "block";
+    }
+    document.getElementById("new-item").onsubmit = addFormItem;
 }
